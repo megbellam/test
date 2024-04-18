d8930577fbd409905de8288da4e2916c80d93e49
megbellam <58276976+megbellam@users.noreply.github.com>
2024-04-17 22:56:37 -0700

    Update asg5a.js

--- asg5a.js
+++ asg5a.js
@@ -84,7 +84,7 @@ function main() {
 	];
 	const cube2 = new THREE.Mesh( geometry, materialT );
 	scene.add( cube2 );
-	cube2.position.x = 2;
+	cube2.position.x = 3;
 	cube2.position.y = 0;
 	cubes.push( cube2 ); // add to our list of cubes to rotate
 
